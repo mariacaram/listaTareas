76b0bd0ffc106bfc8f95a228f16ac9ce475a9422
Maria <maria.caram@gmail.com>
2022-04-29 18:44:36 -0300

    Creación de estructura

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Titulo from './components/Titulo';
+import Subtitulo from './components/Subtitulo';
+
+function App() {
+  return (
+<>
+<Titulo></Titulo>
+<Subtitulo></Subtitulo>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Subtitulo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subtitulo = () => {
+    return (
+        <div>
+            <h2>Tareas de la Comision</h2>
+        </div>
+    );
+};
+
+export default Subtitulo;
