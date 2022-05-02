ce3330d983db3316f80dbaa7cbe88eb63acdb0e8
Maria <maria.caram@gmail.com>
2022-05-02 20:46:59 -0300

    Se agrega el manejador de eventos para la tarea individual y para el arreglo de tareas

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
+  const [tareaIndividual, setTareaIndividual] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("desde el evento submit");
+    setTareas([...tareas, tareaIndividual]);
+  };
   return (
     <>
-      <form className="container my-5">
+      <form className="container my-5" onSubmit={handleSubmit}>
         <div className="mb-3 d-flex">
           <input
             type="text"
             placeholder="Ingrese una tarea"
             className="form-control"
+            onChange={(e) => setTareaIndividual(e.target.value)}
           ></input>
           <button className="btn btn-outline-light " type="submit">
             {" "}
@@ -17,7 +25,9 @@ const FormTareas = () => {
           </button>
         </div>
       </form>
-      <section className="container"><ListaTareas/></section>
+      <section className="container">
+        <ListaTareas />
+      </section>
     </>
   );
 };
